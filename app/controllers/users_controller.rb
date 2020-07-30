76987d5caa3df0b890f96c55d7610d5077ed5d1b
Raymond Oluoch <rasholuoch@gmail.com>
2020-07-30 14:10:00 +0300

    adding users, user profiles and solving heroku issues

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  
+  def index
+    @users = User.all
+  end
+  
+  # GET to /users/:id
+  def show
+    @user = User.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/about_us' => 'static_pages#about'
   devise_for :users
+  resources :users, only: [:index, :show]
   resources :activities, only: [:new, :create, :index, :show, :edit, :update, :destroy]
 end
